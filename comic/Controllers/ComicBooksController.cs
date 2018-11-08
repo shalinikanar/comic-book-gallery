89029ceb8da246ab1315f2b4fed05fac8e27c1bd
Shalini <shalinisinghus@yahoo.com>
2018-11-08 18:31:09 -0500

    this is my first commit

---
+++ comic/Controllers/ComicBooksController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comic.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            ViewBag.seriesTitle = "The Amazing Spider Man";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = @"<p>Final issue! Witness the final hours of Doctor Octopus’
+             life and his one, last, great act of revenge! Even if Spider-Man survives... 
+              <strong>will Peter Parker?</strong></p>";
+            ViewBag.artists = new string[]
+
+            {
+
+                      "Script: Dan Slott",
+                      "Pencils: Humberto Ramos",
+                      "Inks: Victor Olazaba",
+                      "Colors: Edgar Delgado",
+                      "Letters: Chris Eliopoulos"
+
+             };
+
+            return View();
+
+        }
+           
+    }
+}
